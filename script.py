efe58b738177855d619bd29e05bce27cfdedd8cc
truelore_hzh <hezhonghua126@126.com>
2020-08-18 11:07:33 +0800

    remove unuseful code

--- script.py
+++ script.py
@@ -1,11 +1,5 @@
-import sys
-
-print(sys.version)
-print(sys.executable)
-
-
 def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
+    greeting = '!!!Hello, {}'.format(who_to_greet)
     return greeting
 
 
